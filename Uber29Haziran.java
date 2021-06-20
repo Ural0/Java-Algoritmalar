ff60c10a964c4c4317e13d90e01130eb2f5e72d0
Ural0 <70514044+Ural0@users.noreply.github.com>
2021-06-20 14:55:37 +0300

    Add files via upload

---
+++ Uber29Haziran.java
@@ -0,0 +1,35 @@
+package HaziranAyi;
+
+/**
+ *
+ * @author Mehmet Emin Ural
+ */
+public class Uber29Haziran {
+
+    public static void main(String[] args) {
+//https://www.youtube.com/watch?v=LC0zMe2avvM&list=PLD9b0KFQWLE2qQnT2g7l4748hw3zML4eA&index=5&ab_channel=UzaktanAkademiUzaktanAkademi
+        int[] dizi = {1, 2, 3, 4, 5};
+        
+        
+        int carpim = 1;
+        
+        int soldakiSayiar, sagdakiSayilar;
+        int i, j;
+        soldakiSayiar=1;
+        sagdakiSayilar=1;
+        
+        i = 1;
+        j=dizi.length-2;
+        for (; i <dizi.length; i++,j--) {
+          //  sol*=dizi[i-1];
+                    
+        //    carpim = carpim * i;
+            //System.out.println(dizi[i]);
+          // System.out.println(dizi[i]);
+            System.out.println(dizi[j]);
+        }
+
+        //  System.out.print(carpim);
+    }
+
+}
