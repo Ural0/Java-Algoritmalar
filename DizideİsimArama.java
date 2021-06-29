fa873d7c9d08da1728e2123e4f95523ad9a7c6fe
Ural0 <70514044+Ural0@users.noreply.github.com>
2021-06-29 13:33:35 +0300

    Add files via upload
    İsimler dizisinde isim arama

---
+++ DizideİsimArama.java
@@ -0,0 +1,42 @@
+package HaziranAyi;
+
+/**
+ *
+ * @author Mehmet Emin Ural 
+ */
+public class DizideİsimArama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String isimler[] = {"ali", "mehmet", "oguz", "ramazan","sevval","rotinda"};
+        
+        String kelime = "oguz";
+        if (ara(isimler, kelime)) {
+            System.out.println(kelime + " dizide bulundu");
+        } else {
+            System.out.println(kelime + " dizide bulunamadı");
+        }
+
+        kelime = "metin";
+        if (ara(isimler, kelime)) {
+            System.out.println(kelime + " dizide bulundu");
+        } else {
+            System.out.println(kelime + " dizide bulunamadı");
+        }
+
+    }
+
+    public static boolean ara(String dizi[], String aranan) {
+
+        for (int i = 0; i < dizi.length; i++) {
+            if (aranan.equals(dizi[i])) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
